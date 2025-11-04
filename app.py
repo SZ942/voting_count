edfadc81613073c060c2aa999a7468304a26b3c6
SZ942 <i.wsouthx.denr@gmail.com>
2025-11-04 14:00:25 +0900

    app.py を更新

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 import streamlit as st
-
-st.title("投票証明画像の集計ツール")
-
-uploaded_files = st.file_uploader("証明画像をアップロード", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
-
-if uploaded_files:
-    st.write(f"{len(uploaded_files)} 枚の画像がアップロードされました！")
-import streamlit as st
 import easyocr
 import re
 from PIL import Image, ImageEnhance
